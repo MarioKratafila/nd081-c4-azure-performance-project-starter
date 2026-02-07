628b0b8b71dc4cbaa96f9ee5090c55a690100b2e
Mario Kratafila <kratafila@gmail.com>
2026-02-07 22:56:16 +0100

    fixing bug in logging

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -127,12 +127,15 @@ def index():
             vote2 = r.get(button2).decode('utf-8')
 
             if vote == 'Cats':
-                properties = {'custom_dimensions': {'Cats Vote': vote1}}
-                logger.info('Cats Vote', extra=properties)
-            elif vote == 'Dogs':
-                properties = {'custom_dimensions': {'Dogs Vote': vote2}}
-                logger.info('Dogs Vote', extra=properties)
+            #    properties = {'custom_dimensions': {'Cats Vote': vote1}}
+            #    logger.info('Cats Vote', extra=properties)
+                logger.info('Cats Vote')
 
+            elif vote == 'Dogs':
+            #    properties = {'custom_dimensions': {'Dogs Vote': vote2}}
+            #    logger.info('Dogs Vote', extra=properties)
+                logger.info('Dogs Vote')
+            
             # Return results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
